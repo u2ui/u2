09e0854e96aa8628ea1f2649e1d6f28b8183c133
Tobias Buschor <tobias.buschor@shwups.ch>
2024-05-02 21:58:37 +0200

    chore: Add touch-action property to drag handle for better touch interaction

--- attr/draghandle/draghandle.js
+++ attr/draghandle/draghandle.js
@@ -1,20 +1,40 @@
+import 'https://bernardo-castilho.github.io/DragDropTouch/DragDropTouch.js';
+
 const css = `
 [u2-draghandle] {
     cursor: grab;
+    touch-action: none; /* needed? probably yes */
 }
 `;
 document.head.insertAdjacentHTML('afterbegin', `<style>${css}</style>`);
 
 
-addEventListener('mouseenter', e => {
+const root = document;
+// root.addEventListener("pointerdown",(e)=>{
+//    root.releasePointerCapture(e.pointerId);
+// })
+root.addEventListener('pointerenter', e => {
     if (!e.target.matches('[u2-draghandle]')) return;
     const draggable = e.target.closest('[draggable]');
     if (!draggable) console.warn('u2-draghandle: no draggable parent found');
     if (draggable.getAttribute('draggable') === 'true') console.warn('u2-draghandle: draggable-attribute should be empty or false, but was true');
     draggable.setAttribute('draggable', true);
 },true);
-
-addEventListener('mouseleave', e => {
+root.addEventListener('pointerleave', e => {
     if (!e.target.matches('[u2-draghandle]')) return;
     e.target.closest('[draggable]').setAttribute('draggable', 'false');
 },true);
+
+// desktop only
+// addEventListener('mouseenter', e => {
+//     if (!e.target.matches('[u2-draghandle]')) return;
+//     const draggable = e.target.closest('[draggable]');
+//     if (!draggable) console.warn('u2-draghandle: no draggable parent found');
+//     if (draggable.getAttribute('draggable') === 'true') console.warn('u2-draghandle: draggable-attribute should be empty or false, but was true');
+//     draggable.setAttribute('draggable', true);
+// },true);
+
+// addEventListener('mouseleave', e => {
+//     if (!e.target.matches('[u2-draghandle]')) return;
+//     e.target.closest('[draggable]').setAttribute('draggable', 'false');
+// },true);
