09de8b7180ad7919d1734c000dac5d6d22a035d2
Tobias Buschor <github.com@shwups.ch>
2025-11-25 13:12:17 +0100

    u2-responsive improve

--- el/responsive/responsive.js
+++ el/responsive/responsive.js
@@ -3,75 +3,63 @@ class U2Responsive extends HTMLElement {
         super();
         this._internals = this.attachInternals();
         this.currentLevel = -1;
-        this.lastWidth = 0;
-        this.updating = false;
+        this.lastWidth = Infinity;
+        //this.updating = false;
         this.resizeObserver = new ResizeObserver(() => this.updateStrategy());
     }
 
-    connectedCallback() {
-        this.resizeObserver.observe(this);
-        requestAnimationFrame(() => this.updateStrategy());
-    }
+    connectedCallback() { this.resizeObserver.observe(this); }
+    disconnectedCallback() { this.resizeObserver.disconnect(); }
 
-    disconnectedCallback() {
-        this.resizeObserver.disconnect();
-    }
+    get strategies() { return this.getAttribute('strategies')?.trim().split(/\s+/) || []; }
 
-    get strategies() {
-        return this.getAttribute('strategies')?.trim().split(/\s+/) || [];
+    applyStrategy(type, strategy) {
+        const event = new CustomEvent(`u2-responsive-strategy-${type}`, {
+            detail: { strategy },
+            bubbles: true,
+            cancelable: true
+        });
+        this.dispatchEvent(event) && this._internals.states[type==='add'?'add':'delete'](strategy);
+        // Wait for potential DOM changes //await new Promise(resolve => requestAnimationFrame(resolve));
     }
-
-    async applyStrategies(level) {
+    applyStrategies(level) {
         const strats = this.strategies;
-        const currentActive = new Set([...this._internals.states]);
-        const targetActive = new Set(strats.slice(0, level + 1));
-
+        const current = new Set(this._internals.states);
+        const target  = new Set(strats.slice(0, level + 1));
         // Remove strategies that should be removed
-        for (const strategy of currentActive) {
-            if (!targetActive.has(strategy)) {
-                const event = new CustomEvent('u2-responsive-strategy-remove', {detail: {strategy}, bubbles:true, cancelable:true});
-                const allowed = this.dispatchEvent(event);
-                if (allowed) this._internals.states.delete(strategy);
-                // Wait for potential DOM changes //await new Promise(resolve => requestAnimationFrame(resolve));
-            }
-        }
-
+        for (const s of current) !target .has(s) && this.applyStrategy('remove', s);
         // Add strategies that should be added
-        for (const strategy of targetActive) {
-            if (!currentActive.has(strategy)) {
-                const event = new CustomEvent('u2-responsive-strategy-add', {detail: {strategy}, bubbles:true, cancelable:true});
-                const allowed = this.dispatchEvent(event);
-                if (allowed) this._internals.states.add(strategy);
-                // Wait for potential DOM changes //await new Promise(resolve => requestAnimationFrame(resolve));
-            }
-        }
+        for (const s of target ) !current.has(s) && this.applyStrategy('add', s);
     }
 
-    async updateStrategy() {
-        if (this.updating) return;
-        this.updating = true;
+    updateStrategy() {
+        //if (this.updating) return;
+        //this.updating = true;
 
         const width = this.clientWidth;
         const strats = this.strategies;
-        const growing = width > this.lastWidth;
+        const shrinking = width < this.lastWidth; // need every pixel while shrinking
+        const growing = !shrinking; // need every pixel while shrinking
+        //const growing = width > this.lastWidth + 2; // save resources by not taking every pixel into account while growing, todo, track lastGrowinWidth?
         this.lastWidth = width;
 
         if (growing) { // Growing: try to remove strategies
             while (this.currentLevel >= 0) {
-                await this.applyStrategies(this.currentLevel - 1);
+                this.applyStrategies(this.currentLevel - 1);
                 if (this.scrollWidth > this.clientWidth) { // Doesn't fit, revert
-                    await this.applyStrategies(this.currentLevel);
+                    this.applyStrategies(this.currentLevel);
                     break;
                 }
                 this.currentLevel--;
             }
-        } else { // Shrinking: apply strategies
+        }
+        if (shrinking) { // Shrinking: apply strategies
             while (this.scrollWidth > this.clientWidth && this.currentLevel < strats.length - 1) {
                 this.currentLevel++;
-                await this.applyStrategies(this.currentLevel);
+                this.applyStrategies(this.currentLevel);
             }
         }
-        this.updating = false;
+        //this.updating = false;
     }
 
 }
